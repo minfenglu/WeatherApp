08d76e6eae5f499623f5fac1ad8f553936723d3d
minfenglu <contact.minfeng@gmail.com>
2019-07-25 13:23:57 -0700

    add api call to fetch the temperature

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,13 +1,49 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
+  getInitialState: function() {
+    return {
+      isLoading: false,
+    }
+  },
+  handleSearch: function (location) {
+    var updates = {};
+    var that = this;
+    this.setState({isLoading: true});
+    updates.location = location;
+    openWeatherMap.getTemp(location).then(
+      function (temp) {
+        updates.temp = temp;
+        updates.isLoading = false;
+        that.setState(updates);
+      },
+      function (errorMessage) {
+        that.setState({isLoading: false});
+        alert(errorMessage);
+      });
+    this.setState(updates);
+  },
   render: function () {
+    var {location, temp, isLoading} = this.state;
+    function renderMessage () {
+      if (isLoading){
+        return(
+          <h3>Fetching weather...</h3>
+        )
+      }
+      else if (temp && location){
+        return(
+          <WeatherMessage location={location} temp={temp}/>
+        )
+      }
+    }
     return(
       <div>
-        <WeatherForm/>
-        <WeatherMessage/>
+        <WeatherForm onSearch={this.handleSearch}/>
+        {renderMessage()}
       </div>
     )
   }
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,8 +2,9 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
   render: function () {
+    var {location, temp} = this.props;
     return (
-      <h4> Weather Message Component </h4>
+      <h4> {location} is now {temp}. </h4>
     )
   }
 })
