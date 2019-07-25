afa9b34b85b43c309ecd43889e4ed8e681a6b2bf
minfenglu <contact.minfeng@gmail.com>
2019-07-25 10:58:16 -0700

    add skeleton components

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var WeatherMessage = React.createClass({
+  render: function () {
+    return (
+      <h4> Weather Message Component </h4>
+    )
+  }
+})
+
+module.exports = WeatherMessage;
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,14 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
+      Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
+      Weather: 'app/components/Weather.jsx',
+      WeatherForm: 'app/components/WeatherForm.jsx',
+      WeatherMessage: 'app/components/WeatherMessage.jsx',
+      About: 'app/components/About.jsx',
+      Examples: 'app/components/Examples.jsx',
+      openWeatherMap: 'app/api/openWeatherMap.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
@@ -15,7 +23,7 @@ module.exports = {
       {
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015', 'stage-0']
         },
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
