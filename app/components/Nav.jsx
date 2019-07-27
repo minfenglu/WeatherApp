907ffb227603518105aa868cddad9370a18fa888
minfenglu <contact.minfeng@gmail.com>
2019-07-27 16:07:08 -0700

    style nav bar using foundation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,11 +4,21 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   render: function () {
     return(
-      <div>
-        <h3>Nav Component</h3>
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Weather App</li>
+            <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+            <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+            <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li><input type="search" placeholder="Search"/></li>
+            <li><button type="button" className="button">Search</button></li>
+          </ul>
+        </div>
       </div>
     )
   }
